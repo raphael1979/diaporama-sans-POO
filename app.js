e574e51940f6f2cf97fbaac3c21883918037adf4
raphael <moudens.raphael@orange.fr>
2017-01-22 15:31:06 +0100

    modif html&ajout fichier text.json app.js style.css

---
+++ app.js
@@ -0,0 +1,13 @@
+$.getJSON("text.json", function(){
+	var 
+}
+$(document).foundation({
+  orbit: {
+    animation: 'slide',
+    timer_speed: 1000,
+    pause_on_hover: true,
+    animation_speed: 500,
+    navigation_arrows: true,
+    bullets: false
+  }
+});
